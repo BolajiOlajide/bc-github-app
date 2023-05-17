885c6872380c6576bff8fc83a441dd26348ccc1e
Becca Steinbrecher <becca.steinbrecher@sourcegraph.com>
2023-05-17 16:51:49 -0700

    gql implementation

--- go.mod
+++ go.mod
@@ -14,10 +14,12 @@ require (
 	github.com/go-chi/cors v1.2.1 // indirect
 	github.com/golang/protobuf v1.5.2 // indirect
 	github.com/google/go-querystring v1.1.0 // indirect
+	github.com/shurcooL/githubv4 v0.0.0-20230424031643-6cea62ecd5a9 // indirect
+	github.com/shurcooL/graphql v0.0.0-20220606043923-3cf50f8a0a29 // indirect
 	golang.org/x/crypto v0.7.0 // indirect
-	golang.org/x/net v0.9.0 // indirect
+	golang.org/x/net v0.10.0 // indirect
 	golang.org/x/oauth2 v0.7.0 // indirect
-	golang.org/x/sys v0.7.0 // indirect
+	golang.org/x/sys v0.8.0 // indirect
 	google.golang.org/appengine v1.6.7 // indirect
 	google.golang.org/protobuf v1.28.0 // indirect
 )
--- handlers.go
+++ handlers.go
@@ -3,13 +3,15 @@ package main
 import (
 	"bc-github-app/templates"
 	"context"
+	"encoding/base64"
 	"encoding/json"
 	"fmt"
 	"html/template"
-	"log"
 	"net/http"
 
 	"github.com/google/go-github/v52/github"
+	"github.com/shurcooL/githubv4"
+	"golang.org/x/oauth2"
 )
 
 type AuthDetails struct {
@@ -92,25 +94,40 @@ func handleCreateCommit(w http.ResponseWriter, r *http.Request) {
 	}
 
 	ctx := context.Background()
-	client := github.NewTokenClient(ctx, token)
+	// restClient := github.NewTokenClient(ctx, token)
 
-	path := "file.md"
-	content := []byte("# Hola\n* This is a file committed via Github's API\n")
-	message := "hopefully signed commit"
+	src := oauth2.StaticTokenSource(
+		&oauth2.Token{AccessToken: token},
+	)
+	httpClient := oauth2.NewClient(context.Background(), src)
 
-	_, err := createBranch(ctx, client, body.Branch, body.Repo, body.Owner)
+	fmt.Printf("I created the httpclient!")
+
+	graphQLClient := githubv4.NewClient(httpClient)
+
+	fmt.Printf("I created the graphqlclient!")
+
+	// path := "file.md"
+	// content := []byte("# Hola\n* This is a file committed via Github's API\n")
+	// message := "hopefully signed commit"
+
+	// _, err := createBranch(ctx, restClient, body.Branch, body.Repo, body.Owner)
+	// if err != nil {
+	// 	log.Fatalf("error creating branch %s", err.Error())
+	// 	http.Error(w, err.Error(), http.StatusBadRequest)
+	// 	return
+	// }
+
+	// _, _, err = restClient.Repositories.CreateFile(ctx, body.Owner, body.Repo, path, &github.RepositoryContentFileOptions{
+	// 	Message: github.String(message),
+	// 	Content: content,
+	// 	Branch:  github.String(body.Branch),
+	// })
+
+	err := createCommitOnBranch(ctx, graphQLClient, "becca-test", "This is a test!")
 	if err != nil {
-		log.Fatalf("error creating branch %s", err.Error())
-		http.Error(w, err.Error(), http.StatusBadRequest)
-		return
-	}
+		fmt.Printf("There was an error!: %v", err)
 
-	_, _, err = client.Repositories.CreateFile(ctx, body.Owner, body.Repo, path, &github.RepositoryContentFileOptions{
-		Message: github.String(message),
-		Content: content,
-		Branch:  github.String(body.Branch),
-	})
-	if err != nil {
 		http.Error(w, err.Error(), http.StatusBadRequest)
 		return
 	}
@@ -118,15 +135,56 @@ func handleCreateCommit(w http.ResponseWriter, r *http.Request) {
 	w.Write([]byte("success! yaaaay!"))
 }
 
-func createBranch(ctx context.Context, client *github.Client, branch, repo, owner string) (*github.Reference, error) {
+func createCommitOnBranch(ctx context.Context, client *githubv4.Client, branch, message string) (err error) {
+	var mutation struct {
+		CreateCommitOnBranch struct {
+			Commit struct {
+				ID githubv4.ID
+				// Signature struct {
+				// 	Email   string
+				// 	IsValid bool
+				// }
+			}
+		} `graphql:"createCommitOnBranch(input: $input)"`
+	}
+	input := githubv4.CreateCommitOnBranchInput{
+		Branch: githubv4.CommittableBranch{
+			RepositoryNameWithOwner: githubv4.NewString(githubv4.String("st0nebraker/food-diary")),
+			BranchName:              githubv4.NewString(githubv4.String(branch)),
+		},
+		Message: githubv4.CommitMessage{
+			Headline: *githubv4.NewString(githubv4.String(message)),
+		},
+		FileChanges: &githubv4.FileChanges{
+			Additions: &[]githubv4.FileAddition{
+				{
+					Path:     "docs/README.txt",
+					Contents: githubv4.Base64String(base64.StdEncoding.EncodeToString([]byte("Hello world!\n"))),
+				},
+			},
+		},
+		ExpectedHeadOid: githubv4.GitObjectID("d849462e17cda8ad9cc07ef38f3160f2c768154e"),
+	}
+
+	err = client.Mutate(context.Background(), &mutation, input, nil)
+	if err != nil {
+		return err
+		// Handle error.
+	}
+	// fmt.Printf("Commit was signed by %v, right? %v!\n", mutation.CreateCommitOnBranch.Commit.Signature.Email, mutation.CreateCommitOnBranch.Commit.Signature.IsValid)
+
+	return nil
+}
+
+func createBranch(ctx context.Context, restClient *github.Client, branch, repo, owner string) (*github.Reference, error) {
 	// Fetching the latest commit on the master branch
-	ref, _, err := client.Git.GetRef(ctx, owner, repo, "refs/heads/main")
+	ref, _, err := restClient.Git.GetRef(ctx, owner, repo, "refs/heads/master")
 	if err != nil {
 		return nil, err
 	}
 
 	// Creating a new branch
-	newRef, _, err := client.Git.CreateRef(ctx, owner, repo, &github.Reference{
+	newRef, _, err := restClient.Git.CreateRef(ctx, owner, repo, &github.Reference{
 		Ref:    github.String(fmt.Sprintf("refs/heads/%s", branch)),
 		Object: &github.GitObject{SHA: ref.Object.SHA},
 	})
